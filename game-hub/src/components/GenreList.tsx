7edd78450f6010117ab3caad8ed88f40a1e3dc0b
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-05 13:37:53 -0400

    display genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <li key={genre.id}>
+          <HStack paddingY={1}>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </li>
       ))}
-    </ul>
+    </List>
   );
 };
 
